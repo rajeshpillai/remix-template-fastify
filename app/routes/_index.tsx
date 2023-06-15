12d5b812c912fee0e984eb306790659a2432513d
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-06-15 08:37:53 +0530

    Cleanup for base template of Remix with fastify, patch-package and pm2

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,4 @@
 import type { V2_MetaFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -8,21 +7,10 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
-export const loader = async () => {
-  const resp = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data =  await resp.json();
-  console.log("loader: Data: ", new Date(), data[0]);
-  return data;
-};
-
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
-  console.log("Render: DataXX: ", new Date().toISOString(), data[0]);
-  
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix 🎉 118</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>Welcome to Remix 🎉</h1>
     </div>
   );
 }
